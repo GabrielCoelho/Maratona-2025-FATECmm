3dae7e3155bfde2649fd50887af41e92b67143de
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2025-04-27 22:56:08 -0300

    feat: Exercício D

---
+++ maratona2025/src/main/java/br/com/devcoelho/maratona/D.java
@@ -0,0 +1,36 @@
+package br.com.devcoelho.maratona;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * Problema D {@summary} -> Desenvolva um algoritmo que identifique e contabilize a frequência de
+ * cada elemento único em um fluxo de dados numéricos.
+ *
+ * @author Gabriel Coelho Soares
+ * @since 2025
+ */
+public class D {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int quantity = Integer.valueOf(scanner.nextLine());
+    TreeMap<Integer, Integer> result = new TreeMap<>();
+    String fullValues = scanner.nextLine();
+    int[] valuesSplitted =
+        Arrays.stream(fullValues.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+    for (int i = 0; i < valuesSplitted.length; i++) {
+      if (i >= quantity) break;
+      result.put(valuesSplitted[i], result.getOrDefault(valuesSplitted[i], 0) + 1);
+    }
+
+    for (Map.Entry<Integer, Integer> show : result.entrySet()) {
+      int value = show.getKey();
+      int frequency = show.getValue();
+
+      System.out.println(value + " " + frequency);
+    }
+  }
+}
