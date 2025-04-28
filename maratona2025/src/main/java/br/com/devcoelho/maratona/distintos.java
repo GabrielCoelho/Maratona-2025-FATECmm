757a4baee4a3386ffc01acf173da34a8e14c97c4
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2025-04-28 09:34:28 -0300

    Update distintos.java
    adding documentation

--- maratona2025/src/main/java/br/com/devcoelho/maratona/distintos.java
+++ maratona2025/src/main/java/br/com/devcoelho/maratona/distintos.java
@@ -2,6 +2,14 @@ package br.com.devcoelho.maratona;
 
 import java.util.*;
 
+/**
+ * Problema G {@summary} -> Você recebe uma lista de n elementos e precisa determinar quantos
+ * pares (x,y) distintos podem ser formados escolhendo dois elementos diferentes da lista.
+ * O resultado é a combinação matemática de n elementos tomados 2 a 2.
+ *
+ * @author Gabriel Coelho Soares
+ * @since 2025
+ */
 public class distintos {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
