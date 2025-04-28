21a78dfb335a92c6b566719d8846d396e890ada1
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2025-04-28 00:10:05 -0300

    feat(G): Exercício G - depois de compreender, foi mais fácil do que eu pensava

---
+++ maratona2025/src/main/java/br/com/devcoelho/maratona/G.java
@@ -0,0 +1,22 @@
+package br.com.devcoelho.maratona;
+
+import java.util.*;
+
+public class G {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int listSize = scanner.nextInt();
+    // O resultado sempre será a soma de todos os valores anteriores ao listSize
+    int count = (listSize * (listSize - 1)) / 2;
+
+    // apenas para armazenar, não será necessario
+    int[] numbers = new int[listSize];
+    for (int i = 0; i < listSize; i++) {
+      numbers[i] = scanner.nextInt();
+    }
+
+    System.out.println(count);
+    scanner.close();
+  }
+}
