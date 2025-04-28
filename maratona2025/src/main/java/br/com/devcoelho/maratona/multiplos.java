a5adf64282d2ae726e9e9bd36b90cc371522da50
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2025-04-28 09:33:20 -0300

    Update multiplos.java

--- maratona2025/src/main/java/br/com/devcoelho/maratona/multiplos.java
+++ maratona2025/src/main/java/br/com/devcoelho/maratona/multiplos.java
@@ -3,6 +3,14 @@ package br.com.devcoelho.maratona;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Problema F {@summary} -> Você tem uma lista de inteiros e precisa determinar quantos números
+ * na lista são múltiplos de 3. O programa deve receber o tamanho da lista, seguido pelos números,
+ * e retornar a contagem de múltiplos de 3.
+ *
+ * @author Gabriel Coelho Soares
+ * @since 2025
+ */
 public class multiplos {
 
   public static void main(String[] args) {
